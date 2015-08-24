09f84ab9f370966443900995d538851e2649f578
noprom <tyee.noprom@qq.com>
2015-08-24 11:50:58 +0800

    Initial Commit

---
+++ HuntChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HuntChat
+//
+//  Created by noprom on 15/8/24.
+//  Copyright © 2015年 noprom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
